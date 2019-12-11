a9a1ef2a9b5ffb0a5fc2a51c0481d843b4a8342d
benjamineavenson <eavensonbenjamin@gmail.com>
2019-12-11 13:55:50 -0800

    linted other files

--- app/calendar-meteor/imports/ui/Event.js
+++ app/calendar-meteor/imports/ui/Event.js
@@ -4,35 +4,40 @@ import { modifyEventFetch } from './App.js';
 
 export default class Event extends Component {
 
-  handleDelete(){
+  handleDelete() {
     const id = this.props.id;
     const event = modifyEventFetch(id);
     const name = event.name;
-    if(confirm("Are you sure you wish to delete " + name + "?")){
+    if (confirm('Are you sure you wish to delete ' + name + '?')) {
       deleteEvent(id);
       this._reactInternalFiber._debugOwner.stateNode.forceUpdate();
     }
   }
 
-  handleModify(){
-    const id=this.props.id;
+  handleModify() {
+    const id = this.props.id;
     this._reactInternalFiber._debugOwner.stateNode._reactInternalFiber._debugOwner.stateNode.setState({
       eventMod: modifyEventFetch(id),
       page: 2,
     });
   }
 
-  renderButtons(){
-    if(this.props.buttons === 0){
-      return("");
+  renderButtons() {
+    if (this.props.buttons === 0) {
+      return ('');
     }
-    return(<div><button className="ui button" onClick={this.handleModify.bind(this)}><i className="edit icon"></i>Modify Event</button>
-      <button className="ui red button" onClick={this.handleDelete.bind(this)}><i className="close icon"></i>Delete Event</button></div>);
+    return (<div>
+      <button className="ui button" onClick={this.handleModify.bind(this)}><i className="edit icon"></i>Modify Event
+      </button>
+      <button className="ui red button" onClick={this.handleDelete.bind(this)}><i className="close icon"></i>Delete
+        Event
+      </button>
+    </div>);
   }
 
   render() {
     return (
-        <div className="eight wide column">
+      <div className="eight wide column">
         <div className="ui fluid card">
           <div className="header">{this.props.name}</div>
           <div className="ui grid">
@@ -51,7 +56,7 @@ export default class Event extends Component {
             </div>
           </div>
         </div>
-        </div>
+      </div>
     );
   }
 }
